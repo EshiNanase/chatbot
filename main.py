2e2b9e38d7a2ae0545f4e3676c524413d2803313
Nanashi <alksndr.zln@gmail.com>
2023-02-07 00:54:59 +0300

    Added requirements

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
 URL_REVIEWS = 'https://dvmn.org/api/user_reviews/'
 URL_LP = 'https://dvmn.org/api/long_polling/'
 TOKEN_DEVMAN = os.environ['TOKEN_DEVMAN']
@@ -17,9 +16,9 @@ TIMESTAMP = 0
 PAYLOAD = {'timestamp': TIMESTAMP}
 
 TOKEN_TG = os.environ['TOKEN_TG']
-CHAT_ID = 362428124
 
 bot = telegram.Bot(token=TOKEN_TG)
+CHAT_ID = bot.get_updates()[-1].message.chat_id
 
 while True:
     try:
@@ -29,14 +28,12 @@ while True:
         response = requests.get(URL_LP, headers=HEADERS, params=PAYLOAD).json()
 
         if response['status'] == 'found':
-            text = f'Преподаватель проверил {response["new_attempts"][0]["lesson_title"]}\n' \
+            text = f'Преподаватель проверил: "{response["new_attempts"][0]["lesson_title"]}"\n' \
                    f'{response["new_attempts"][0]["lesson_url"]}\n\n'
-
             if response["new_attempts"][0]["is_negative"] is True:
                 text += 'Ноль ошибок, едем дальше!'
             else:
                 text += 'Правь код, позорник'
-
             bot.send_message(chat_id=CHAT_ID, text=text)
             break
         else:
