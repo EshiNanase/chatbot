77688ddd51ac422ba5b8001ed2d89a29f9e52920
Nanashi <alksndr.zln@gmail.com>
2023-02-07 14:06:18 +0300

    Packed everything in def main

--- main.py
+++ main.py
@@ -2,47 +2,48 @@ import time
 import requests
 import telegram
 import os
-
 from dotenv import load_dotenv
-load_dotenv()
 
-URL_REVIEWS = 'https://dvmn.org/api/user_reviews/'
-URL_LP = 'https://dvmn.org/api/long_polling/'
-TOKEN_DEVMAN = os.environ['TOKEN_DEVMAN']
-HEADERS = {
-    'Authorization': TOKEN_DEVMAN
-}
-TIMESTAMP = 0
-PAYLOAD = {'timestamp': TIMESTAMP}
 
-TOKEN_TG = os.environ['TOKEN_TG']
+def main() -> None:
+    load_dotenv()
 
-bot = telegram.Bot(token=TOKEN_TG)
-CHAT_ID = bot.get_updates()[-1].message.chat_id
+    url_reviews = 'https://dvmn.org/api/user_reviews/'
+    url_lp = 'https://dvmn.org/api/long_polling/'
+    token_devman = os.environ['TOKEN_DEVMAN']
+    headers = {
+        'Authorization': token_devman
+    }
+    timestamp = 0
+    payload = {'timestamp': timestamp}
 
-while True:
-    try:
-        print('Making a request...')
-        requests.get(URL_REVIEWS, headers=HEADERS)
-        print('Success!\n')
-        response = requests.get(URL_LP, headers=HEADERS, params=PAYLOAD).json()
+    token_tg = os.environ['TOKEN_TG']
 
-        if response['status'] == 'found':
-            text = f'Преподаватель проверил: "{response["new_attempts"][0]["lesson_title"]}"\n' \
-                   f'{response["new_attempts"][0]["lesson_url"]}\n\n'
-            if response["new_attempts"][0]["is_negative"] is True:
-                text += 'Ноль ошибок, едем дальше!'
+    bot = telegram.Bot(token=token_tg)
+    chat_id = bot.get_updates()[-1].message.chat_id
+
+    while True:
+        try:
+            requests.get(url_reviews, headers=headers)
+            response = requests.get(url_lp, headers=headers, params=payload).json()
+
+            if response['status'] == 'found':
+                text = f'Преподаватель проверил: "{response["new_attempts"][0]["lesson_title"]}"\n' \
+                       f'{response["new_attempts"][0]["lesson_url"]}\n\n'
+                if response["new_attempts"][0]["is_negative"] is False:
+                    text += 'Ноль ошибок, едем дальше!'
+                else:
+                    text += 'Правь код, позорник'
+                bot.send_message(chat_id=chat_id, text=text)
             else:
-                text += 'Правь код, позорник'
-            bot.send_message(chat_id=CHAT_ID, text=text)
-            break
-        else:
-            bot.send_message(chat_id=CHAT_ID, text='Ничего :(')
-            TIMESTAMP = response['timestamp_to_request']
+                bot.send_message(chat_id=chat_id, text='Ничего :(')
+                timestamp = response['timestamp_to_request']
 
-    except requests.exceptions.ReadTimeout:
-        print('ReadTimeout\n')
-        time.sleep(5)
-    except requests.exceptions.ConnectionError:
-        print('No internet connection!\n')
-        time.sleep(5)
+        except requests.exceptions.ReadTimeout:
+            time.sleep(5)
+        except requests.exceptions.ConnectionError:
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
